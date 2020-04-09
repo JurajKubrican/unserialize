4f4d069a222441bdc6f9f5de6a8d110eba8ce41e
Personal <kubrican.juraj@gmail.com>
2020-04-09 15:44:33 +0200

    Dragon auth token formatter

--- src/components/TextOutput/TextOutput.jsx
+++ src/components/TextOutput/TextOutput.jsx
@@ -1,23 +1,33 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-import {php, es} from '../../lib/VarWriter.js'
+import { php, es } from "../../lib/VarWriter.js";
 
+const getAuthToken = (data) => {
+  const result = {
+    customerId: "customerId",
+    authorization: data.token_type + " " + data.access_token,
+    idToken: data.id_token,
+  };
 
+  return JSON.stringify(result);
+};
 class TextOutput extends Component {
-    render() {
-        return (
-            <div>
-            <textarea name="name" id="" cols="30" rows="10">
-                {php(this.props.data)}
-            </textarea>
-                <textarea name="name" id="" cols="30" rows="10">
-                {es(this.props.data)}
-            </textarea>
-            </div>
-
-        )
-
-    }
+  render() {
+    const { data } = this.props;
+    return (
+      <>
+        <textarea name="name" cols="50" rows="20" value={php(data)} readOnly />
+        <textarea name="name" cols="50" rows="20" value={es(data)} readOnly />
+        <textarea
+          name="name"
+          cols="50"
+          rows="20"
+          value={getAuthToken(data)}
+          readOnly
+        />
+      </>
+    );
+  }
 }
 
 export default TextOutput;
--- src/components/VisualizerApp.jsx
+++ src/components/VisualizerApp.jsx
@@ -1,108 +1,122 @@
-import React, {Component} from 'react';
-import Visualizer from './Visualizer/Visualizer';
-import TextOutput from './TextOutput/TextOutput';
+import React, { Component } from "react";
+import Visualizer from "./Visualizer/Visualizer";
+import TextOutput from "./TextOutput/TextOutput";
 
 import TextField from "@material-ui/core/es/TextField/TextField";
 import Button from "@material-ui/core/es/Button/Button";
 import Paper from "@material-ui/core/es/Paper/Paper";
 import Grid from "@material-ui/core/es/Grid/Grid";
 
+import exampleData from "./example.json";
 
-import exampleData from './example.json'
-
-
-import strToObj from './strToObj'
+import strToObj from "./strToObj";
 
 const style = {
-    flexRow: {
-        display: 'flex',
-        flexDirection: 'row',
-    },
-    flexCol: {
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    button: {
-        display: 'block',
-        marginBottom: '5px',
-        marginRight: '10px',
-    },
-    panel: {
-        minHeight: '60px',
-    },
-    textarea:{
-        width:'100%',
-    }
+  flexRow: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  flexCol: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  button: {
+    display: "block",
+    marginBottom: "5px",
+    marginRight: "10px",
+  },
+  panel: {
+    minHeight: "60px",
+  },
+  textarea: {
+    width: "100%",
+  },
 };
 
 class VisualizerApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      url: "https://tools.knet.sk/example.xml",
+      text: JSON.stringify(exampleData),
+    };
 
-    constructor() {
-        super();
-        this.state = {
-            url: 'https://tools.knet.sk/example.xml',
-            text: JSON.stringify(exampleData),
-        };
+    this.fetchUrl = this.fetchUrl.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-        this.fetchUrl = this.fetchUrl.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+  fetchUrl() {
+    if (!this.state.url.match(/https?:\/\/\w+/i)) {
+      this.setState({ url: "http://" + this.state.url });
     }
+    fetch(
+      "https://wt-kubrican_juraj-gmail_com-0.sandbox.auth0-extend.com/urlBounce?url=" +
+        this.state.url,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ text: data.text });
+      })
+      .catch(() => {});
+  }
 
-    fetchUrl() {
-        if (!this.state.url.match(/https?:\/\/\w+/i)) {
-            this.setState({url: 'http://' + this.state.url});
-        }
-        fetch('https://wt-kubrican_juraj-gmail_com-0.sandbox.auth0-extend.com/urlBounce?url=' + this.state.url, {
-            method: 'GET',
-        }).then((response) => {
-            return response.json()
-        }).then((data) => {
-            this.setState({text: data.text});
-        }).catch(() => {
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-        })
-    }
+  render() {
+    const [data, format] = strToObj(this.state.text);
 
-    handleChange(e) {
-        this.setState({[e.target.name]: e.target.value});
-    }
-
-    render() {
-        const [data, format] = strToObj(this.state.text);
-
-        return (
-            <Paper>
-                <Grid container spacing={24}>
-                    <Grid item xs={12} sm={4}>
-                        <Grid item sm={4}>
-                            <textarea name={"text"} style={style.textarea} onChange={this.handleChange}
-                                      value={this.state.text}/>
-                        </Grid>
-                        <Grid item sm={4}>
-                            <TextField name={'url'} helperText="URL" value={this.state.url}
-                                       onChange={this.handleChange}/>
-                            <Button style={style.button} onClick={this.fetchUrl}>Refresh</Button>
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <div style={style.flexRow}>
-                            <div style={style.panel}>
-                                {format}
-                                <Visualizer data={data}/>
-                            </div>
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <div style={style.flexRow}>
-                            <div style={style.panel}>
-                                <TextOutput data={data}/>
-                            </div>
-                        </div>
-                    </Grid>
-                </Grid>
-            </Paper>
-        );
-    }
+    return (
+      <Paper>
+        <Grid container spacing={8}>
+          <Grid item xs={12} sm={4}>
+            <Grid item sm={4}>
+              <textarea
+                name={"text"}
+                style={style.textarea}
+                onChange={this.handleChange}
+                value={this.state.text}
+              />
+            </Grid>
+            <Grid item sm={4}>
+              <TextField
+                name={"url"}
+                helperText="URL"
+                value={this.state.url}
+                onChange={this.handleChange}
+              />
+              <Button style={style.button} onClick={this.fetchUrl}>
+                Refresh
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <div style={style.flexRow}>
+              <div style={style.panel}>
+                {format}
+                <Visualizer data={data} />
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+        <Grid container spacing={8}>
+          <Grid item xs={12} sm={4}>
+            <div style={style.flexRow}>
+              <div style={style.panel}>
+                <TextOutput data={data} />
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </Paper>
+    );
+  }
 }
 
 export default VisualizerApp;
