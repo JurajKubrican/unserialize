53c4dd58f4ecc3b08a2e5289b659fd89e973c904
Personal <kubrican.juraj@gmail.com>
2020-04-09 15:15:26 +0200

    Fix missing default warning

--- src/lib/VarWriter.js
+++ src/lib/VarWriter.js
@@ -1,56 +1,64 @@
-import esRules from './es.json'
-import phpRules from './php.json'
-
+import esRules from "./es.json";
+import phpRules from "./php.json";
 
 const traverse = (data, rules, depth) => {
-    if (data === undefined) {
-        return '';
-    }
-    let result = depth ? '' : rules.start;
+  if (data === undefined) {
+    return "";
+  }
+  let result = depth ? "" : rules.start;
 
-    switch (data.constructor) {
-        case Array:
-            result += rules.arrStart;
-            result += data.map((data) => {
-                return traverse(data, rules, depth + 1)
-            }, rules, depth).join(rules.arrDivider);
-            result += rules.arrEnd;
-            break;
+  switch (data.constructor) {
+    case Array:
+      result += rules.arrStart;
+      result += data
+        .map(
+          (data) => {
+            return traverse(data, rules, depth + 1);
+          },
+          rules,
+          depth
+        )
+        .join(rules.arrDivider);
+      result += rules.arrEnd;
+      break;
 
-        case Object:
-            result += rules.objStart;
-            for (const key in data) {
-                if (!data.hasOwnProperty(key)) {
-                    continue;
-                }
-                result += rules.objKeyDelim + key + rules.objKeyDelim + rules.objMid + traverse(data[key], rules, depth + 1) + rules.objDivider;
-            }
-            result += rules.objEnd;
-            break;
+    case Object:
+      result += rules.objStart;
+      for (const key in data) {
+        if (!data.hasOwnProperty(key)) {
+          continue;
+        }
+        result +=
+          rules.objKeyDelim +
+          key +
+          rules.objKeyDelim +
+          rules.objMid +
+          traverse(data[key], rules, depth + 1) +
+          rules.objDivider;
+      }
+      result += rules.objEnd;
+      break;
 
-        case String:
-            result += rules.strDelim + data + rules.strDelim;
-            break;
+    case String:
+      result += rules.strDelim + data + rules.strDelim;
+      break;
 
-        case Number:
-            result += rules.intDelim + data + rules.intDelim;
-            break;
-    }
+    case Number:
+    default:
+      result += rules.intDelim + data + rules.intDelim;
+      break;
+  }
 
-    result += depth ? '' : rules.end;
-    return result
+  result += depth ? "" : rules.end;
+  return result;
 };
 
 const php = (data) => {
-    return traverse(data, phpRules, 0)
+  return traverse(data, phpRules, 0);
 };
-
 
 const es = (data) => {
-    return traverse(data, esRules, 0)
+  return traverse(data, esRules, 0);
 };
 
-export {
-    es,
-    php
-};
+export { es, php };
